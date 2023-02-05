47fe49d02e6af2fb078e3db5eaa0651b66d69298
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-04 21:32:56 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -7,14 +7,19 @@ response = requests.get(url)
 if response.status_code == 200:
     contents = response.json()
     m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
+    lists = []
+    for m3u_file in m3u_files:
+        m3u_url = m3u_file["download_url"]
+        m3u_response = requests.get(m3u_url)
+
+        if m3u_response.status_code == 200:
+            lists.append((m3u_file["name"], m3u_response.text))
+            
+    lists = sorted(lists, key=lambda x: x[0])
 
     with open("lista1.M3U", "w") as f:
-        for m3u_file in m3u_files:
-            m3u_url = m3u_file["download_url"]
-            m3u_response = requests.get(m3u_url)
-
-            if m3u_response.status_code == 200:
-                f.write(m3u_response.text)
+        for l in lists:
+            f.write(l[1])
 
 else:
     print("Error retrieving contents from GitHub API")
