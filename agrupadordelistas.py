ed6b35a029537b8fd6c411b3e50df6a461b30363
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-06 03:42:01 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,25 +1,30 @@
 import requests
+from bs4 import BeautifulSoup
+import datetime
+import streamlink
 
-url = "https://api.github.com/repos/guiworldtv/STR-YT/contents"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
-response = requests.get(url)
+m3u8_file = open("lista2str2.m3u", "w")
 
-if response.status_code == 200:
-    contents = response.json()
-    m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
-    lists = []
-    for m3u_file in m3u_files:
-        m3u_url = m3u_file["download_url"]
-        m3u_response = requests.get(m3u_url)
+url = "https://tviplayer.iol.pt/"
 
-        if m3u_response.status_code == 200:
-            lists.append((m3u_file["name"], m3u_response.text))
-            
-    lists = sorted(lists, key=lambda x: x[0])
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, "html.parser")
 
-    with open("lista1.M3U", "w") as f:
-        for l in lists:
-            f.write(l[1])
+video_link = soup.find("a", href="/direto/TVI")["href"]
+video_link = f"https://tviplayer.iol.pt{video_link}"
 
-else:
-    print("Error retrieving contents from GitHub API")
+video_url = streamlink.streams(video_link)["best"].url if streamlink.streams(video_link) else None
+
+programs = soup.find_all("div", class_="programCover")
+program_names = [program.find_parent("div", class_="program").find("div", class_="titlePrograma").text for program in programs]
+program_images = [program["style"].split("url(")[1].split(")")[0] for program in programs]
+
+for name, image in zip(program_names, program_images):
+    m3u8_file.write(f"#EXTINF:-1 group-title=\"TVI PLAYER\" tvg-logo=\"{image}\",{name}\n{video_url}\n")
+    m3u8_file.write("\n")
+
+m3u8_file.close()
