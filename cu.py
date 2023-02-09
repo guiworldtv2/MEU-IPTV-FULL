83a51b1b1da2792bdfec60fa7a0854dd80db28f1
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-09 19:43:49 -0300

    Update cu.py

--- cu.py
+++ cu.py
@@ -1,12 +1,35 @@
-import os
 import streamlink
+import moviepy.editor as mp
 import subprocess
+import os
 import time
 
+# URL do stream
 url = "https://5a7d54e35f9d2.streamlock.net/avt7/avt7.stream/chunklist_w127968702.m3u8"
-stream = streamlink.streams(url)["best"]
 
-while True:
-    filename = time.strftime("%Y-%m-%d_%H-%M-%S.png")
+# Tempo de captura de cada frame (em segundos)
+capture_time = 5
+
+# Número de frames a serem capturados
+num_frames = 60
+
+# Iniciar o stream
+streams = streamlink.streams(url)
+stream = streams["best"]
+
+# Iniciar a captura de frames
+for i in range(num_frames):
+    filename = "frame_{}.png".format(i)
     subprocess.call(["ffmpeg", "-i", stream.url, "-vframes", "1", filename])
-    time.sleep(30)
+    time.sleep(capture_time)
+
+# Criar o vídeo a partir dos frames capturados
+filenames = ["frame_{}.png".format(i) for i in range(num_frames)]
+clip = mp.ImageSequenceClip(filenames, fps=1 / capture_time)
+clip.write_videofile("timelapse.mp4")
+
+# Limpar os arquivos de frame
+for filename in filenames:
+    os.remove(filename)
+
+   
