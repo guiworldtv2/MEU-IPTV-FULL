d69b6fe5c6b633404c971e2fab135399871ed171
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-09 12:51:02 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,27 +1,26 @@
 import requests
 
-url = "https://api.github.com/repos/guiworldtv/STR-YT/contents"
+repo_urls = [    "https://api.github.com/repos/guiworldtv/STR-YT/contents",    "https://api.github.com/repos/guiworldtv/STR-YTDDD/contents",    "https://api.github.com/repos/guiworldtv/STR-YTDddaADDD/contents"]
 
-response = requests.get(url)
+lists = []
+for url in repo_urls:
+    response = requests.get(url)
 
-if response.status_code == 200:
-    contents = response.json()
-    m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
-    lists = []
-    for m3u_file in m3u_files:
-        m3u_url = m3u_file["download_url"]
-        m3u_response = requests.get(m3u_url)
+    if response.status_code == 200:
+        contents = response.json()
+        m3u_files = [content for content in contents if content["name"].endswith(".m3u")]
 
-        if m3u_response.status_code == 200:
-            lists.append((m3u_file["name"], m3u_response.text))
-            
-    lists = sorted(lists, key=lambda x: x[0])
+        for m3u_file in m3u_files:
+            m3u_url = m3u_file["download_url"]
+            m3u_response = requests.get(m3u_url)
 
-    with open("lista1.M3U", "w") as f:
-        for l in lists:
-            f.write(l[1])
+            if m3u_response.status_code == 200:
+                lists.append((m3u_file["name"], m3u_response.text))
+    else:
+        print(f"Error retrieving contents from {url}")
 
-else:
-    print("Error retrieving contents from GitHub API")
+lists = sorted(lists, key=lambda x: x[0])
 
-
+with open("lista1.M3U", "w") as f:
+    for l in lists:
+        f.write(l[1])
