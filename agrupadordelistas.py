0f861aabd566d62ae353cdf29445793ed9152ee7
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-09 15:22:42 -0300

    Update agrupadordelistas.py

--- agrupadordelistas.py
+++ agrupadordelistas.py
@@ -1,6 +1,6 @@
 import requests
 
-repo_urls = [    "https://api.github.com/repos/vijay6672/YT2M3U/contents",    "https://api.github.com/repos/guiworldtv/YT2M3U/contents",    "https://api.github.com/repos/guiworldtv/GERADOR-DE-MODULOS/contents"]
+repo_urls = [    "https://api.github.com/repos/vijay6672/YT2M3U/contents",    "https://api.github.com/repos/guiworldtv/YT2M3U/contents",    "https://api.github.com/repos/guiworldtv/MEU-IPTV-FULL/contents"]
 
 lists = []
 for url in repo_urls:
