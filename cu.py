11b314f6314edb32819841b1276e02b540387742
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-10 06:21:54 -0300

    Update cu.py

--- cu.py
+++ cu.py
@@ -8,15 +8,12 @@ import time
 url = "https://5a7d54e35f9d2.streamlock.net/avt7/avt7.stream/chunklist_w127968702.m3u8"
 
 # Tempo de captura de cada frame (em segundos)
-capture_time = 5
+capture_time = 2
 
 # Número de frames a serem capturados
-num_frames = 120
+num_frames = 240
 
-# Duração do vídeo final (em segundos)
-video_duration = 400
-
-# Taxa de quadros por segundo (FPS) da timelapse
+# FPS da timelapse
 fps = 30
 
 # Iniciar o stream
@@ -26,22 +23,14 @@ stream = streams["best"]
 # Iniciar a captura de frames
 for i in range(num_frames):
     filename = "frame_{}.png".format(i)
-    subprocess.call(["ffmpeg", "-i", stream.url, "-vframes", "1", filename])
+    subprocess.call(["ffmpeg", "-i", stream.url, "-vframes", "1", "-r", str(fps), filename])
     time.sleep(capture_time)
 
 # Criar o vídeo a partir dos frames capturados
 filenames = ["frame_{}.png".format(i) for i in range(num_frames)]
 clip = mp.ImageSequenceClip(filenames, fps=fps)
-
-# Ajustar a duração do vídeo
-clip = clip.set_duration(video_duration)
-
-# Exportar o vídeo
-clip.write_videofile("timelapse.mp4", preset='ultrafast', fps=fps)
+clip.write_videofile("timelapse.mp4", preset='ultrafast')
 
 # Limpar os arquivos de frame
 for filename in filenames:
     os.remove(filename)
-
-# Baixar e salvar apenas o áudio do vídeo de exemplo
-subprocess.call(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "https://www.youtube.com/watch?v=RfYmwwQnPR8"])
